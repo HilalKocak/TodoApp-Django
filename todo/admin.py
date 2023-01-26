640a837b1de3ea87938bdead7f2b96b9253256b7
Hilal38 <hhilalkocak@gmail.com>
2023-01-26 15:32:43 +0300

    created table

---
+++ todo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Todo
+class TodoAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ]
+
+admin.site.register(Todo, TodoAdmin)
